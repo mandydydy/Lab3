b5b31d395095ac68316b424b1ec0d1ffa8a205d4
TURuibo <ruibo.tu@gmail.com>
2016-10-08 09:06:22 +0200

    log2 log10 ln
    in order to get the result of tutorial

--- discriminant.m
+++ discriminant.m
@@ -1,6 +1,7 @@
 function g=discriminant(data, mu, sigma, p)
     [num_row_data,num_col_data]=size(data);
-    N=num_col_data;
+    [num_class, num_feature,T]=size(sigma);
+    N=num_feature;
     M=num_row_data;
     C=length(p);
     %initialize g
@@ -12,12 +13,12 @@ function g=discriminant(data, mu, sigma, p)
            sum_sigma=0;
            sum_G=0;
            for n=1:N
-              sum_sigma=sum_sigma+log10(sigma(i,n));
+              sum_sigma=sum_sigma+log(sigma(i,n));
            end
            for n=1:N
-               sum_G=sum_G+(data(x,n)-mu(i,n))^2/(2*sigma(i,n));
+               sum_G=sum_G+(data(x,n)-mu(i,n))^2/(2*(sigma(i,n))^2);
            end
-           g(x,i)=log10(p(i))-sum_sigma-sum_G;
+           g(x,i)=log(p(i))-sum_sigma-sum_G;
        end
     end
 end
